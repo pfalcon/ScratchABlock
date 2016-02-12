df30197bc5ff5ed3f065c8ef8dc06d464acfddd7
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-02-12 23:13:14 +0200

    dataflow: Make solve() a method, support analysis with transfer function.
    This introduces AnalysisBase class where .solve() belongs, and supports
    full dataflow analysis where working sets depend on node content (and
    this different for entry and exit of a node).

--- dataflow.py
+++ dataflow.py
@@ -1,45 +1,80 @@
-def solve(cfg, analysis):
-    "Solve dataflow for analysis object on a graph."
-    analysis.init(cfg)
-    prop = analysis.node_prop
+class AnalysisBase:
 
-    changed = True
-    while changed:
-        changed = False
+    # Set to False for backward analysis
+    forward = True
+    # Set to name of node "in" state
+    node_prop_in = None
+    # Set to name of node "out" state
+    node_prop_out = None
 
-        for node, info in cfg.iter_sorted_nodes():
-            if analysis.forward:
-                sources = cfg.pred(node)
-            else:
-                sources = cfg.succ(node)
-            new = analysis.new_state(node, sources)
-            if new != info[prop]:
-                info[prop] = new
-                changed = True
+    def __init__(self, graph):
+        self.g = graph
+
+    def solve(self):
+        "Solve dataflow analysis."
+        self.init()
+        prop_in = self.node_prop_in
+        prop_out = self.node_prop_out
+
+        changed = True
+        while changed:
+            changed = False
+
+            for node, info in self.g.iter_sorted_nodes():
+                if self.forward:
+                    sources = self.g.pred(node)
+                else:
+                    sources = self.g.succ(node)
+
+                if prop_out:
+                    info[prop_out] = self.transfer(node, info[prop_in])
+
+                if sources:
+                    # If there're no "sources" for this node, it's an initial node,
+                    # and should keep it's "in" set (which may be non-empty).
+                    new = self.join(node, sources)
+                    if new != info[prop_in]:
+                        info[prop_in] = new
+                        changed = True
+
+    def transfer(self, node, src_state):
+        """Transfer function. Computes next state of a node, based on
+        source state. For forward analisys, source state is 'in' state,
+        next state is 'out' state. For backward analysis, vice-versa.
+        Note that this function should not be concerned with direction
+        of analysis, it's just fed with 'source' state by the solver.
+
+        Default implementation does nothing, and is suitable for analyses
+        which don't depend on node "content", only on overall graph
+        structure (control flow analyses vs data flow analyses).
+        """
+        return src_state
+
+    def join(self, node, source_nodes):
+        raise NotImplementedError
 
 
-class DominatorAnalysis:
+class DominatorAnalysis(AnalysisBase):
     "Encapsulation of dataflow analysis to discover graph node dominators."
 
     forward = True
-    node_prop = "dom"
+    node_prop_in = "dom"
 
-    def init(self, cfg):
+    def init(self):
         "Entry node is set to itself, the rest - to graph's all nodes."
-        self.cfg = cfg
-        entry = cfg.entries()
+        entry = self.g.entries()
         assert len(entry) == 1
         entry = entry[0]
-        all_nodes = set(cfg.nodes())
-        for node, info in cfg.iter_nodes():
+        all_nodes = set(self.g.nodes())
+        for node, info in self.g.iter_nodes():
             if node == entry:
-                info[self.node_prop] = {node}
+                info[self.node_prop_in] = {node}
             else:
-                info[self.node_prop] = all_nodes
+                info[self.node_prop_in] = all_nodes
 
-    def new_state(self, node, sources):
-        if sources:
-            state = set.intersection(*(self.cfg.get_node_attr(x, self.node_prop) for x in sources))
+    def join(self, node, source_nodes):
+        if source_nodes:
+            state = set.intersection(*(self.g.get_node_attr(x, self.node_prop_in) for x in source_nodes))
         else:
             state = set()
         return state | {node}
