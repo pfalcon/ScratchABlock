373b4ff4ecb178085d3915150fadfe82888533f2
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-05-02 00:08:31 +0300

    parse_asm: Switch to argparse, allow to dump repr() format of instructions.
    Using --repr option, helpful for debugging parser or getting acquainted with
    internal representation.

--- parse_asm.py
+++ parse_asm.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 import sys
+import argparse
 from parser import *
+import core
 import dot
 
 
-p = Parser(sys.argv[1])
+argp = argparse.ArgumentParser(description="Parse and dump PseudoC program")
+argp.add_argument("file", help="Input file in PseudoC format")
+argp.add_argument("--repr", action="store_true", help="Dump __repr__ format of instructions")
+args = argp.parse_args()
+
+p = Parser(args.file)
 cfg = p.parse()
 print("Labels:", p.labels)
 
+if args.repr:
+    core.SimpleExpr.simple_repr = False
+
 print("Basic blocks:")
-dump_bblocks(cfg)
+dump_bblocks(cfg, printer=repr if args.repr else str)
 
 with open(sys.argv[1] + ".0.dot", "w") as f: dot.dot(cfg, f)
