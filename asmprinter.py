92e8da599d8ed24ce69d92fc2013fa8068be9a28
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-05-06 00:33:33 +0300

    asmprinter: Factor out RoundtripPrinter from parse_asm.py.

---
+++ asmprinter.py
@@ -0,0 +1,59 @@
+import sys
+import argparse
+from parser import *
+import core
+import dot
+from xform import foreach_inst
+
+
+class RoundtripPrinter(CFGPrinter):
+
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        self.addr_width = 8
+        self.inst_indent = 4
+        self.inst_printer = self.print_with_addr
+        self.referenced_labels = set()
+        self.get_jump_labels()
+
+    def format_addr(self, addr, extra=0):
+        return addr.ljust(self.addr_width + extra)
+
+    def get_jump_labels(self):
+        def collect_labels(inst):
+            addr = inst.jump_addr()
+            if addr is not None:
+                self.referenced_labels.add(addr)
+        foreach_inst(self.cfg, collect_labels)
+
+    def print_with_addr(self, inst):
+        return self.format_addr(inst.addr, self.inst_indent) + " " + str(inst)
+
+    def resolve_label(self, addr):
+        return self.cfg.parser.label_from_addr(addr)
+
+    def print_label(self):
+        # If there was symbolic label originally, print it
+        # otherwise, skip it if it's not referenced by insts
+        # (like bblock start labels).
+        label = self.cfg.parser.label_from_addr(self.addr)
+        if label == self.addr:
+            if self.addr not in self.referenced_labels:
+                return
+        print("%s %s:" % (self.format_addr(self.addr), label), file=self.stream)
+
+    def print_header(self):
+        pass
+    def print_separator(self):
+        pass
+    def print_trailer(self):
+        pass
+
+    def print(self):
+        core.ADDR.resolver = self.resolve_label
+        super().print()
+
+
+def dump_asm(cfg):
+    p = RoundtripPrinter(cfg)
+    p.print()
