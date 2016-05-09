e51363b53d21f56049b2870a24d54ba9f5e89287
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-05-10 00:19:57 +0300

    xform: DCE: Use bblock's live-out sets if available.
    Effectively, doing global dead code elimination.

--- xform.py
+++ xform.py
@@ -83,21 +83,29 @@ def loop_single_entry(cfg):
 
 
 def dead_code_elimination(bblock):
+    def make_dead(insts, idx):
+        org_inst = insts[idx]
+        if org_inst.side_effect():
+            org_inst.dest = None
+        else:
+            insts[idx] = Inst(None, "DEAD", [])
+            insts[idx].comments["org_inst"] = org_inst
+
     vars = bblock.defs()
     for v in vars:
         last = None
         for i, inst in enumerate(bblock.items):
+            if v in inst.args:
+                last = None
             if inst.dest == v:
                 if last is not None:
-                    org_inst = bblock.items[last]
-                    if org_inst.side_effect():
-                        org_inst.dest = None
-                    else:
-                        bblock.items[last] = Inst(None, "DEAD", [])
-                        bblock.items[last].comments["org_inst"] = org_inst
+                    make_dead(bblock.items, last)
                 last = i
-            elif v in inst.args:
-                last = None
+        node = bblock.cfg[bblock.addr]
+        live_out = node.get("live_out")
+        if last is not None and live_out is not None:
+            if v not in live_out:
+                make_dead(bblock.items, last)
 
 
 def bblock_const_propagation(bblock):
