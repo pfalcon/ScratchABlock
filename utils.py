0f5dba21aed25a695401455e0d65a3953b445f97
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-05-14 03:14:50 +0300

    utils: Add set_union/set_intersection.
    Python's set.union, etc. is unfortunately not a class method, but a normal
    method, [and if used as unbound method], requires at least one argument
    (a set).

--- utils.py
+++ utils.py
@@ -6,3 +6,21 @@ def pairwise(iterable):
             yield (prev, it)
         prev = it
     yield (it, None)
+
+
+def set_union(*sets):
+    # Python's set.union is unfortunately not a class method,
+    # but a normal method, [and if used as unbound method],
+    # requires at least one argument (set).
+    if sets:
+        return set.union(*sets)
+    return set()
+
+
+def set_intersection(*sets):
+    # Python's set.intersection is unfortunately not a class method,
+    # but a normal method, [and if used as unbound method],
+    # requires at least one argument (set).
+    if sets:
+        return set.intersection(*sets)
+    return set()
