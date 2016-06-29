bf6108db61f7d2c0adc56a4a402bea1a4ef2eeaa
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-06-29 23:08:08 +0300

    arch: Describe calling conventions for Xtensa call0.

--- arch.py
+++ arch.py
@@ -1,2 +1,26 @@
+from core import REG
+
 BITNESS = 32
 ENDIANNESS = "LITTLE"
+
+
+def reg_range(first, last):
+    return {REG("a%d" % x) for x in range(first, last + 1)}
+
+ALL_REGS = reg_range(0, 15)
+
+
+def call_args(addr):
+    return reg_range(2, 7)
+
+def call_ret(addr):
+    return reg_range(2, 5)
+
+def call_save(addr):
+    return reg_range(12, 15) | {REG("sp")}
+
+def call_uses(addr):
+    return call_args(addr)
+
+def call_defs(addr):
+    return call_ret(addr) | (ALL_REGS - call_save(addr))
