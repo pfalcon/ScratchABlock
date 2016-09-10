a185bcfd755734916905cc2a16e7ffceffd31c32
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-09-10 22:28:22 +0300

    xform_expr: Add expr_simplify_add().

--- xform_expr.py
+++ xform_expr.py
@@ -1,8 +1,18 @@
 from core import *
+import arch
 
 
 def is_expr_2args(e):
     return is_expr(e) and len(e.args)
+
+
+def mod_add(a, b, bits=arch.BITNESS):
+    "Addition modulo power of 2, but preserve sign."
+    s = a + b
+    if s < 0:
+        return s % 2**bits - 2**bits
+    else:
+        return s % 2**bits
 
 
 def expr_xform(e, func):
@@ -45,3 +55,22 @@ def expr_associative_add(e):
             new_args = e.args[0].args.copy()
             new_args.extend(e.args[1:].copy())
             return EXPR("+", new_args)
+
+
+def expr_simplify_add(e):
+    if is_expr(e) and e.op == "+":
+        new_args = []
+        val = 0
+        for a in e.args:
+            if is_value(a):
+                val = mod_add(val, a.val)
+            else:
+                new_args.append(a)
+        if new_args:
+            if val != 0:
+                new_args.append(VALUE(val))
+            if len(new_args) == 1:
+                return new_args[0]
+            return EXPR("+", new_args)
+        else:
+            return VALUE(val)
