366e24028204e6cee8c6999c76af67580070c314
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-01-01 12:10:15 +0300

    dom: Adhoc/optimized routines for dominators.

---
+++ dom.py
@@ -0,0 +1,37 @@
+"Adhoc/optimized routines for dominators."
+
+def intersect(g, i, j):
+    finger1 = i
+    finger2 = j
+    while finger1 != finger2:
+        while -g[finger1]["dfsno"] > -g[finger2]["dfsno"]:
+            finger1 = g[finger1]["idom"]
+        while -g[finger2]["dfsno"] > -g[finger1]["dfsno"]:
+            finger2 = g[finger2]["idom"]
+    return finger1
+
+
+# Cooper 9.5.2
+def compute_idom(g):
+    for n, info in g.iter_nodes():
+        info["idom"] = None
+
+    entry = g.entry()
+    g[entry]["idom"] = entry
+    changed = True
+    while changed:
+        changed = False
+        it = iter(g.iter_rev_postorder())
+        # Skip root
+        next(it)
+        for n, data in it:
+            preds_rpo = sorted([(-g[p]["dfsno"], p) for p in g.pred(n)])
+            new_idom = preds_rpo[0][1]
+            for _, p in preds_rpo[1:]:
+                if g[p]["idom"] is not None:
+                    new_idom = intersect(g, p, new_idom)
+            if data["idom"] != new_idom:
+                data["idom"] = new_idom
+                changed = True
+
+    g[entry]["idom"] = None
