2f9b22f77b4d8f6599382c3f1cd90a4e31e89221
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-02-27 01:45:12 +0300

    script_decompile.py: Initial decompilation script for apply_xform.

---
+++ script_decompile.py
@@ -0,0 +1,56 @@
+from dom import *
+from dataflow import *
+from xform import *
+from decomp import *
+
+
+def propagate(cfg):
+    analyze_reach_defs(cfg)
+    make_du_chains(cfg)
+
+    # Choose one, expr_propagation is required for true decompilation
+    #const_propagation(cfg)
+    #copy_propagation(cfg)
+    #mem_propagation(cfg)
+    expr_propagation(cfg)
+
+
+def dce(cfg):
+    analyze_live_vars(cfg)
+    foreach_bblock(cfg, dead_code_elimination)
+
+
+def structure(cfg):
+    apply_iterative(match_seq, (cfg,))
+    apply_iterative(match_if, (cfg,))
+    apply_iterative(match_ifelse, (cfg,))
+    apply_iterative(match_seq, (cfg,))
+    apply_iterative(match_ifelse, (cfg,))
+    apply_iterative(match_if_else_ladder, (cfg,))
+
+
+def apply(cfg):
+    #
+    # Data flow
+    #
+
+    foreach_bblock(cfg, sub_const_to_add)
+    insert_sp0(cfg)
+
+    propagate(cfg)
+
+    # Estimate args only at the beginning of processing
+    estimate_args(cfg)
+
+    dce(cfg)
+
+    #
+    # Control flow
+    #
+
+    foreach_bblock(cfg, remove_trailing_jumps)
+    remove_jump_over_jump(cfg)
+
+    cfg.number_postorder()
+    compute_idom(cfg)
+    structure(cfg)
