c95c98da723e6aec27fc83d328986e7921161635
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-03-26 00:25:33 +0300

    script_callgraph: Script for call graph information collection.

---
+++ script_callgraph.py
@@ -0,0 +1,16 @@
+# Minimal processing required to collect calls addresses (and
+# indirect function references) for call graph generation.
+from xform import *
+
+def apply(cfg):
+    # Various algos below require single-exit CFG
+#    cfg_single_exit(cfg)
+
+    analyze_reach_defs(cfg)
+    const_propagation(cfg)
+    #copy_propagation(cfg)
+    #mem_propagation(cfg)
+    #expr_propagation(cfg)
+
+    collect_calls(cfg)
+    collect_func_refs(cfg)
