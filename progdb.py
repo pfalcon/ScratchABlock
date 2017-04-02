be393c29dae3e8a302fb1d96ba7c42baa141be0d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-04-02 14:49:50 +0300

    progdb: New standalone module for dealing with "program database".
    Added functions for dealing with structures, should migrate funcdb
    functions from arch.py too.

---
+++ progdb.py
@@ -0,0 +1,23 @@
+
+struct_types = {}
+struct_instances = {}
+
+
+def set_struct_types(data):
+    global struct_types
+    struct_types = data
+
+
+def set_struct_instances(data):
+    global struct_instances
+    struct_instances = data
+
+
+def get_struct_types():
+    global struct_types
+    return struct_types
+
+
+def get_struct_instances():
+    global struct_instances
+    return struct_instances
