301ff9368ac4d7ef2925496efe7c1d5f14442fa7
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-04-17 12:03:12 +0300

    funcdb_util: called_by: Also make "calls_unk" annotation.
    Contains unknown functions called by the given function.

--- funcdb_util.py
+++ funcdb_util.py
@@ -16,11 +16,13 @@ args = argp.parse_args()
 with open(args.file) as f:
     FUNC_DB = yaml.load(f)
 
-if args.command == "label2addr":
-    label2addr = {}
+label2addr = {}
 
-    for addr, props in FUNC_DB.items():
-        label2addr[props["label"]] = addr
+for addr, props in FUNC_DB.items():
+    label2addr[props["label"]] = addr
+
+
+if args.command == "label2addr":
 
     for addr, props in FUNC_DB.items():
         props["calls_addr"] = [label2addr.get(x, x) for x in props["calls"]]
@@ -31,16 +33,22 @@ elif args.command == "addr2label":
         props["calls"] = [FUNC_DB[x]["label"] if x in FUNC_DB else x for x in props["calls_addr"]]
 
 elif args.command == "called_by":
-    calls = {}
+    called = {}
+
     for addr, props in FUNC_DB.items():
+        calls_unk = []
         name = props["label"]
         for callee in props.get("calls", []):
-            calls.setdefault(callee, set()).add(name)
+            called.setdefault(callee, set()).add(name)
+            if callee not in label2addr:
+                calls_unk.append(callee)
+        if calls_unk:
+            props["calls_unk"] = calls_unk
 
     for addr, props in FUNC_DB.items():
         name = props["label"]
-        if name in calls:
-            props["called_by"] = calls[name]
+        if name in called:
+            props["called_by"] = called[name]
 
 else:
     argp.error("Unknown command: " + args.command)
