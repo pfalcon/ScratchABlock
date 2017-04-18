72e36f02be36219e412495d661bf288595cd95d9
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-04-19 02:23:07 +0300

    core: Move stable representation functions to utils module.
    Also, extend it to support sequences with recursive sets/dicts.

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import re
+
 
 # We need a stable output to compare with expected patterns. But mark
 # places where sorting is optional with 'maybesorted' so it can be
@@ -31,3 +33,58 @@ def set_intersection(*sets):
     if sets:
         return set.intersection(*sets)
     return set()
+
+
+def natural_sort_key(s):
+    if not isinstance(s, str):
+        return s
+    arr = re.split("([0-9]+)", s)
+    return [int(x) if x.isdigit() else x for x in arr]
+
+
+def repr_stable_dict(d):
+    res = "{"
+    comma = False
+    for k, v in sorted(d.items()):
+        if comma:
+            res += ", "
+        res += "%r: %r" % (k, v)
+        comma = True
+    res += "}"
+    return res
+
+def repr_stable_set(d):
+    if not d:
+        return "set()"
+    res = "{"
+    comma = False
+    for k in sorted(list(d), key=lambda x: natural_sort_key(repr(x))):
+        if comma:
+            res += ", "
+        res += "%r" % (k,)
+        comma = True
+    res += "}"
+    return res
+
+
+def repr_stable_seq(d):
+    res = "(" if isinstance(d, tuple) else "["
+    comma = False
+    for el in d:
+        if comma:
+            res += ", "
+        res += repr_stable(el)
+        comma = True
+    res += ")" if isinstance(d, tuple) else "]"
+    return res
+
+
+def repr_stable(v):
+    if isinstance(v, dict):
+        return repr_stable_dict(v)
+    elif isinstance(v, set):
+        return repr_stable_set(v)
+    elif isinstance(v, (list, tuple)):
+        return repr_stable_seq(v)
+    else:
+        return str(v)
