d012c5d2e2310362c32ad64ec3fec94e41cfe736
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-04-19 02:24:27 +0300

    yamlutils: Update for natural_sort_key() moved to utils.

--- yamlutils.py
+++ yamlutils.py
@@ -1,10 +1,10 @@
 import yaml
 
-import core
+import utils
 
 
 def set_representer(dumper, data):
-    return dumper.represent_list(sorted(list(data), key=core.natural_sort_key))
+    return dumper.represent_list(sorted(list(data), key=utils.natural_sort_key))
 
 
 yaml.add_representer(set, set_representer)
