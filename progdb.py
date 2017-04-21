9d5703419487e2c1299e28a5969c8a2fdf9c9fa0
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-04-22 01:29:43 +0300

    apply_xform: Move funcdb handling to progdb.

--- progdb.py
+++ progdb.py
@@ -3,6 +3,13 @@
 # for analysis passes. Among them: function database (funcdb.yaml),
 # databases of structures, etc.
 #
+import os.path
+
+import yaml
+import yamlutils
+
+import core
+
 
 FUNC_DB = {}
 FUNC_DB_BY_ADDR = {}
@@ -16,6 +23,42 @@ def set_funcdb(db):
     for addr, props in list(db.items()):
         FUNC_DB[props["label"]] = props
     FUNC_DB_BY_ADDR = db
+
+
+REG_PROPS = [
+    "callsites_live_out", "modifieds", "preserveds", "reach_exit", "args", "estimated_args",
+    "returns",
+]
+
+def preprocess_funcdb(FUNC_DB):
+    for addr, props in FUNC_DB.items():
+        for prop in REG_PROPS:
+            if prop in props:
+                props[prop] = set(core.REG(x) for x in props[prop])
+
+
+def postprocess_funcdb(FUNC_DB):
+    for addr, props in FUNC_DB.items():
+        for prop in REG_PROPS:
+            if prop in props:
+                props[prop] = sorted([x.name for x in props[prop]], key=core.natural_sort_key)
+
+
+def load_funcdb(fname):
+    with open(fname) as f:
+        FUNC_DB = yaml.load(f)
+        preprocess_funcdb(FUNC_DB)
+        set_funcdb(FUNC_DB)
+
+
+def save_funcdb(fname, backup=True):
+    global FUNC_DB_BY_ADDR
+    postprocess_funcdb(FUNC_DB_BY_ADDR)
+    if backup and os.path.exists(fname):
+        os.rename(fname, fname + ".bak")
+
+    with open(fname, "w") as f:
+        yaml.dump(FUNC_DB_BY_ADDR, f)
 
 
 def set_struct_types(data):
