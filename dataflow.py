7285bf7d4427cd78c608326caf1e5ab30fb5b19b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-05-17 01:10:16 +0300

    dataflow: LiveVarAnalysis: Overestimate, not underestimate function live-out.

--- dataflow.py
+++ dataflow.py
@@ -191,7 +191,15 @@ class LiveVarAnalysis(GenKillAnalysis):
 
         for node, info in self.g.iter_nodes():
             info[self.node_prop_in] = set()
-            info[self.node_prop_out] = set()
+            if node == exit:
+                if "modifieds" in self.g.props:
+                    info[self.node_prop_out] = self.g.props["modifieds"]
+                elif "reach_exit" in self.g.props:
+                    info[self.node_prop_out] = self.g.props["reach_exit"]
+                else:
+                    info[self.node_prop_out] = set()
+            else:
+                info[self.node_prop_out] = set()
 
             bblock = info["val"]
 
