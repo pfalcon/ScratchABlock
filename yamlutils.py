fc0a2da9c939c4caf039f59a93e55509dbba0fea
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-05-18 00:20:10 +0300

    yamlutils: Write funcdb YAML file so "label" property was first.

--- yamlutils.py
+++ yamlutils.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 import yaml
 
 import utils
@@ -8,3 +10,23 @@ def set_representer(dumper, data):
 
 
 yaml.add_representer(set, set_representer)
+
+
+# Workaround pyyaml idiocy of forcibly sort dicts by key
+class DictRenderWrapper:
+
+    def __init__(self, d):
+        self.d = d
+
+    def __iter__(self):
+        return iter(self.d.items())
+
+
+def dict_representer(dumper, data):
+    ordered = OrderedDict()
+    if "label" in data:
+        ordered["label"] = data["label"]
+    ordered.update(sorted(data.items()))
+    return dumper.represent_dict(DictRenderWrapper(ordered))
+
+yaml.add_representer(dict, dict_representer)
