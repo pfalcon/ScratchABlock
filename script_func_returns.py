7f12b8c1e9f788c197d6ca976806312d5b5fb18b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-05-21 11:52:51 +0300

    script_func_returns: Add collect_returns and init() with clear_call_live_out.

--- script_func_returns.py
+++ script_func_returns.py
@@ -3,6 +3,10 @@ from dataflow import *
 
 import script_preserveds
 
+def init():
+    clear_call_live_out()
+
 def apply(cfg):
     script_preserveds.apply(cfg)
     collect_call_live_out(cfg)
+    collect_returns()
