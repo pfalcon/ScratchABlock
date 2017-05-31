31db2a027c539a04a66ed935bcbbc2db3d5b0c5d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-06-01 00:04:20 +0300

    script_callgraph: Run cfg_single_entry.

--- script_callgraph.py
+++ script_callgraph.py
@@ -3,10 +3,12 @@
 from xform import *
 
 def apply(cfg):
-    # Various algos below require single-exit CFG
-#    cfg_single_exit(cfg)
-    # And single entry
+    # Various algos below don't work with no explicit entry in CFG
+    cfg_single_entry(cfg)
+    # Also don't work with >1 entries
     remove_unreachable_entries(cfg)
+    # Also don't work unless there's a single exit
+#    cfg_single_exit(cfg)
 
     analyze_reach_defs(cfg)
     const_propagation(cfg)
