61c43e07c57711c6e279c5cb81b61db797653e84
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-06-02 00:23:30 +0300

    xform_expr_infer: Add simple inference engine.
    Uses pattern matching with unification and imperative templating. Helps
    to express simple expression transformation rules in a declarative way.

---
+++ xform_expr_infer.py
@@ -0,0 +1,85 @@
+from core import *
+
+
+# Capturing var
+class V:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "V(%r)" % self.name
+
+
+class Failed(Exception):
+    pass
+
+
+def _uni(ex, pat, ctx):
+#    print("_uni: %r vs %r" % (ex, pat))
+
+    if isinstance(pat, V):
+        if pat.name in ctx and ctx[pat.name] != ex:
+            raise Failed
+        ctx[pat.name] = ex
+
+    elif type(ex) is type(pat):
+        if isinstance(ex, EXPR):
+            if ex.op == pat.op and len(ex.args) == len(pat.args):
+                for i in range(len(ex.args)):
+                    _uni(ex.args[i], pat.args[i], ctx)
+            else:
+                raise Failed
+        else:
+            if ex == pat:
+                return True
+            raise Failed
+
+    else:
+        raise Failed
+
+
+def uni(ex, pat):
+    ctx = {}
+    _uni(ex, pat, ctx)
+    return ctx
+
+RULES = []
+
+RULES.append((
+    EXPR("-", V("x"), V("x")),
+    lambda W: VALUE(0)
+))
+
+RULES.append((
+    EXPR("^", V("x"), V("x")),
+    lambda W: VALUE(0)
+))
+
+RULES.append((
+    EXPR("+", V("x"), VALUE(0)),
+    lambda W: W["x"]
+))
+
+RULES.append((
+    EXPR("^", V("x"), VALUE(0)),
+    lambda W: W["x"]
+))
+
+
+def simplify(ex):
+    for pat, prod in RULES:
+        #print("Trying", pat)
+        try:
+            ctx = uni(ex, pat)
+        except Failed:
+            #print("Failed")
+            continue
+        return prod(ctx)
+
+
+if __name__ == "__main__":
+    ex = EXPR("-", REG("a1"), REG("a1"))
+    ex = EXPR("^", REG("a1"), REG("a1"))
+    ex = EXPR("^", REG("a1"), VALUE(1))
+    print(simplify(ex))
