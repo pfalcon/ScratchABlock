3c0318cb101cb5fa00c2fa59ae25dcabdedfefdf
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-09-03 11:55:48 +0300

    script_i_func_args_returns: Remove legacy passes.

--- script_i_func_args_returns.py
+++ script_i_func_args_returns.py
@@ -3,11 +3,7 @@ from dataflow import *
 
 import script_i_preserveds
 
-def init():
-    clear_call_live_out()
-
 def apply(cfg):
     script_i_preserveds.apply(cfg)
     collect_args(cfg)
     collect_call_live_out(cfg)
-    collect_returns()
