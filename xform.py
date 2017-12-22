1689846672466cb268d8819973828a1800d2c94c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-12-23 00:43:29 +0200

    xform: Add "booleanize_cond" pass.
    Previously was done during parsing. Transform conditions to be boolean
    relations, e.g.:
    
    ($a1)  -> ($a1 != 0)
    (!$a2) -> ($a2 == 0)

--- xform.py
+++ xform.py
@@ -388,6 +388,23 @@ def collect_state_in(cfg):
     return changed
 
 
+def booleanize_cond(cfg):
+    "Make conditions be of bool type (have bool operation)."
+
+    def booleanize(inst):
+        if inst.op == "if":
+            cond = inst.args[0]
+            if not cond.is_relation():
+                e = cond.expr
+                if is_expr(e) and e.op == "!":
+                    new = EXPR("==", e.args[0], VALUE(0, 0))
+                else:
+                    new = EXPR("!=", e, VALUE(0, 0))
+                cond.expr = new
+
+    foreach_inst(cfg, booleanize)
+
+
 def normalize_cond(cfg):
     "Normalize conditions so constants were on the right side."
 
