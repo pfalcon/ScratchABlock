72af40140a0b42228b25556c9a0ec843aee0d11f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-12-24 01:34:51 +0200

    apply_xform: Explicitly set default loglevel to INFO.

--- apply_xform.py
+++ apply_xform.py
@@ -41,7 +41,7 @@ def parse_args():
     argp.add_argument("--annotate-calls", action="store_true", help="annotate calls if uses/defs")
     argp.add_argument("--repr", action="store_true", help="dump __repr__ format of instructions and other objects")
     argp.add_argument("--debug", action="store_true", help="produce debug files")
-    argp.add_argument("--log-level", help="set logging level")
+    argp.add_argument("--log-level", metavar="LEVEL", default="INFO", help="set logging level (default: %(default)s)")
     args = argp.parse_args()
 
     if args.repr:
