179c1c18f0b5aeeef6da9b061c9aa110426cfb18
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-12-24 11:50:46 +0200

    dot: Show dfsno_exit node property.

--- dot.py
+++ dot.py
@@ -27,6 +27,8 @@ def dot(graph, out=sys.stdout, directed=None, is_cfg=True):
             label = addr
         if "dfsno" in info:
             label += "(#%s)" % info["dfsno"]
+        if info.get("dfsno_exit"):
+            label += "(e#%s)" % info["dfsno_exit"]
         if "idom" in info:
             label += "\nidom: %s" % info["idom"]
         out.write('"%s" [label="%s"]\n' % (addr, label))
