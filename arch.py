3c441f6f94325bdfb90e78c454b99725b68681ba
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-01-17 23:05:48 +0200

    arch: Implement dynamic loading of specific architecture.
    "xtensa" is still hardcoded though.

--- arch.py
+++ arch.py
@@ -1,2 +1,20 @@
-# TODO: Support multiple archs/callconv, load dynamically
-from arch_xtensa import *
+
+arch_m = None
+
+def load_arch(name):
+    global arch_m
+    arch_m = __import__("arch_" + name)
+    for var in dir(arch_m):
+        if var.startswith("__"):
+            continue
+        globals()[var] = getattr(arch_m, var)
+
+
+# TODO: Support multiple archs/callconv, load dynamically based on
+# user options.
+load_arch("xtensa")
+
+
+if __name__ == "__main__":
+    load_arch("xtensa")
+    print(BITNESS)
