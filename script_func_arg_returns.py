001b7c905eee3f97fe5d696ca2958bff79e044ff
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-11-10 23:21:46 +0300

    script_func_arg_returns: Explicitly call calc_callsites_live_out pass.
    Updated similarly to script_func_returns.

--- script_func_arg_returns.py
+++ script_func_arg_returns.py
@@ -1,13 +1,21 @@
 from xform import *
 from dataflow import *
+import xform_inter
 
 import script_preserveds
 
+cg = None
+
+
 def init():
-    clear_call_live_out()
+    global cg
+    cg = xform_inter.build_callgraph()
+
 
 def apply(cfg):
     script_preserveds.apply(cfg)
     collect_call_live_out(cfg)
-    collect_returns()
+
+    xform_inter.calc_callsites_live_out(cg, cfg.props["name"])
+    xform_inter.collect_returns()
     collect_params(cfg)
