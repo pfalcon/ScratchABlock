67cb6ab19ba6be6b4e1fb55608c9b5ccf6a0ce2a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-11-11 13:20:18 +0300

    xform: Reintroduce as just a multiplexor module.
    Just import various xform_* modules and re-exports their passes.

---
+++ xform.py
@@ -0,0 +1,24 @@
+# ScratchABlock - Program analysis and decompilation framework
+#
+# Copyright (c) 2015-2018 Paul Sokolovsky
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Transformation passes multiplexor"""
+
+from xform_expr import *
+from xform_inst import *
+from xform_bblock import *
+from xform_graph import *
+from xform_cfg import *
