7c90b65ac2bd8527019b08f0c877dd2dd758e6ef
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-11-11 13:54:41 +0300

    xform_cfg: Move apply_iterative to xform_utils.

---
+++ xform_utils.py
@@ -0,0 +1,32 @@
+# ScratchABlock - Program analysis and decompilation framework
+#
+# Copyright (c) 2015-2018 Paul Sokolovsky
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Transformation helpers and utilities"""
+
+import logging
+
+
+_log = logging.getLogger(__name__)
+
+
+# Apply tranformation while it's possible
+def apply_iterative(func, args):
+    cnt = 0
+    while func(*args):
+        cnt += 1
+    _log.info("Ran %s %d times" % (func, cnt))
+    return cnt
