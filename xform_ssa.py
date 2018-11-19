f20ffc36df18a8dd05d91868e44efa96e3c298bc
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-11-20 00:11:04 +0300

    xform_ssa: Add rename_ssa_vars_fully_maximal pass.
    As it stands now, it's exact copy of rename_ssa_vars. But the latter is
    actually buggy, written with the assumption of fully maximal form, so
    it will be patched later.

--- xform_ssa.py
+++ xform_ssa.py
@@ -129,6 +129,61 @@ def rename_ssa_vars(cfg, use_addrs=False):
                 inst.args[0].args[my_pos + 1] = new_reg
 
 
+def rename_ssa_vars_fully_maximal(cfg, use_addrs=False):
+
+    def rename_reg(e):
+        if is_reg(e):
+            return REG("%s_%s" % (e.name, var_map[e.name]))
+
+    if use_addrs:
+        var_map = defaultdict(lambda: "0")
+    else:
+        var_map = defaultdict(int)
+
+    # This algorithm could process basic blocks in any order. But for human
+    # consumption, it's better when variable are numbered from the start of
+    # a function.
+    for n in cfg.iter_rev_postorder():
+        bb = cfg[n]["val"]
+
+        # Rename variables within basic block.
+        for inst in bb:
+            # Don't rename args of phi funcs, those names come from
+            # predecessor blocks.
+            if not is_phi(inst):
+                for i, arg in enumerate(inst.args):
+                    inst.args[i] = xform_expr.expr_xform(arg, rename_reg)
+
+            # Rename destination
+            if is_reg(inst.dest):
+                # If it's a register, this defines a new register version
+                if use_addrs:
+                    if is_phi(inst):
+                        suffix = bb.addr + "_phi"
+                    else:
+                        suffix = inst.addr
+                    var_map[inst.dest.name] = suffix
+                else:
+                    var_map[inst.dest.name] += 1
+                inst.dest = rename_reg(inst.dest)
+            else:
+                # Otherwise, it's more complex expression which may contain
+                # reference to reg as a subexpression
+                inst.dest = xform_expr.expr_xform(inst.dest, rename_reg)
+
+        # Now rename arguments of phi functions in successor blocks, in the
+        # position which correspond to the current block's predecessor edge
+        for next_n in cfg.succ(n):
+            my_pos = cfg.pred(next_n).index(n)
+            next_bb = cfg[next_n]["val"]
+            for inst in next_bb:
+                if not is_phi(inst):
+                    break
+                # +1 because first arg is SFUNC("phi")
+                new_reg = rename_reg(inst.args[0].args[my_pos + 1])
+                inst.args[0].args[my_pos + 1] = new_reg
+
+
 def rename_ssa_vars_subscripts(cfg):
     rename_ssa_vars(cfg, False)
 
