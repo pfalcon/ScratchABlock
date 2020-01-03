c62554ea69405a7a0800cedabbe7adaa2b54babe
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-01-04 01:42:34 +0300

    dump_c: Convert to use __main__().

--- dump_c.py
+++ dump_c.py
@@ -7,18 +7,23 @@ from xform import *
 import cprinter
 
 
-p = Parser(sys.argv[1])
-cfg = p.parse()
-#print("Labels:", p.labels)
+def __main__():
+    p = Parser(sys.argv[1])
+    cfg = p.parse()
+    #print("Labels:", p.labels)
 
-cfg.parser = p
-remove_trailing_jumps(cfg)
-cfg.number_postorder()
-Inst.trail = ";"
+    cfg.parser = p
+    remove_trailing_jumps(cfg)
+    cfg.number_postorder()
+    Inst.trail = ";"
 
-#print("Basic blocks:")
-#dump_bblocks(cfg)
+    #print("Basic blocks:")
+    #dump_bblocks(cfg)
 
-#with open(sys.argv[1] + ".0.dot", "w") as f: dot.dot(cfg, f)
+    #with open(sys.argv[1] + ".0.dot", "w") as f: dot.dot(cfg, f)
 
-cprinter.dump_c(cfg)
+    cprinter.dump_c(cfg)
+
+
+if __name__ == "__main__":
+    __main__()
